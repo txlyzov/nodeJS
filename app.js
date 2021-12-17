88709e90bfa3f28b460f90d92700ef0976495900
txlyzov <txlyzov@mail.ru>
2021-12-17 14:47:01 +0300

    simple server

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.end('We are on avtive server page and got status 200!');
+
+  });
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
